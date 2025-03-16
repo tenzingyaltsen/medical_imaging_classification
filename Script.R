833561b2cd30473d7be668874c10eb36c0d5ef19
Tenzin Gyaltsen <tenzingyaltsen16@gmail.com>
2025-03-16 04:22:27 -0400

    Created subdirectory tree for training (75%) and validation (25%) data, and copied over image data from original source folder.

--- Script.R
+++ Script.R
@@ -25,48 +25,52 @@ neu_val_indices <- setdiff(neu_indices, neu_train_indices)
 # Then for eosinophils.
 eos_train_indices <- sample(eos_indices, size = 0.75 * length(eos_indices))
 eos_val_indices <- setdiff(eos_indices, eos_train_indices)
+# These indices need to be converted into a specific format that matches image names.
+neu_train_indices <- sprintf("%05d", neu_train_indices)
+neu_val_indices <- sprintf("%05d", neu_val_indices)
+eos_train_indices <- sprintf("%05d", eos_train_indices)
+eos_val_indices <- sprintf("%05d", eos_val_indices)
 
 # Now let's create the subdirectory tree using the provided code.
 # Define the original data source folder.
-original_dataset_dir <- "~/JPEGImages"
-base_dir <- "~/Downloads/cats_and_dogs_small"
+original_dataset_dir <- file.path(getwd(), "JPEGImages")
+# Define and create the new data source folder.
+base_dir <- file.path(getwd(),"neutrophils_and_eosinophils")
 dir.create(base_dir)
+# Define and create the training folder (within new data source folder).
 train_dir <- file.path(base_dir, "train")
 dir.create(train_dir)
+# Define and create the validation folder (within new data source folder).
 validation_dir <- file.path(base_dir, "validation")
 dir.create(validation_dir)
-test_dir <- file.path(base_dir, "test")
-dir.create(test_dir)
+# No need for test folder as per instructions.
 
-train_cats_dir <- file.path(train_dir, "cats")
-dir.create(train_cats_dir)
-train_dogs_dir <- file.path(train_dir, "dogs")
-dir.create(train_dogs_dir)
-validation_cats_dir <- file.path(validation_dir, "cats")
-dir.create(validation_cats_dir)
-validation_dogs_dir <- file.path(validation_dir, "dogs")
-dir.create(validation_dogs_dir)
-test_cats_dir <- file.path(test_dir, "cats")
-dir.create(test_cats_dir)
-test_dogs_dir <- file.path(test_dir, "dogs")
-dir.create(test_dogs_dir)
+# Create folder for neutrophils within training folder.
+train_neu_dir <- file.path(train_dir, "neutrophil")
+dir.create(train_neu_dir)
+# Create folder for eosinophils within training folder.
+train_eos_dir <- file.path(train_dir, "eosinophil")
+dir.create(train_eos_dir)
+# Create folder for neutrophils within validation folder.
+validation_neu_dir <- file.path(validation_dir, "neutrophil")
+dir.create(validation_neu_dir)
+# Create folder for eosinophils within validation folder.
+validation_eos_dir <- file.path(validation_dir, "eosinophil")
+dir.create(validation_eos_dir)
 
-fnames <- paste0("cat.", 1:1000, ".jpg")
+# Copy over the training images to their respective folders.
+fnames <- paste0("BloodImage_", neu_train_indices, ".jpg")
 file.copy(file.path(original_dataset_dir, fnames),
-          file.path(train_cats_dir))
-fnames <- paste0("cat.", 1001:1500, ".jpg")
+          file.path(train_neu_dir))
+# Note that "BloodImage_00280.jpg" does not exist (manually checked). Oh well!
+fnames <- paste0("BloodImage_", eos_train_indices, ".jpg")
 file.copy(file.path(original_dataset_dir, fnames),
-          file.path(validation_cats_dir))
-fnames <- paste0("cat.", 1501:2000, ".jpg")
+          file.path(train_eos_dir))
+# Copy over the validation images to their respective folders.
+fnames <- paste0("BloodImage_", neu_val_indices, ".jpg")
 file.copy(file.path(original_dataset_dir, fnames),
-          file.path(test_cats_dir))
+          file.path(validation_neu_dir))
+fnames <- paste0("BloodImage_", eos_val_indices, ".jpg")
+file.copy(file.path(original_dataset_dir, fnames),
+          file.path(validation_eos_dir))
 
-fnames <- paste0("dog.", 1:1000, ".jpg")
-file.copy(file.path(original_dataset_dir, fnames),
-          file.path(train_dogs_dir))
-fnames <- paste0("dog.", 1001:1500, ".jpg")
-file.copy(file.path(original_dataset_dir, fnames),
-          file.path(validation_dogs_dir))
-fnames <- paste0("dog.", 1501:2000, ".jpg")
-file.copy(file.path(original_dataset_dir, fnames),
-          file.path(test_dogs_dir))
